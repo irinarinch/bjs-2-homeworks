2e3eb4475ee9002967f42df3fb33423bfb2d5c7d
irinarinch <irinarinch@gmail.com>
2022-08-23 01:15:10 +0800

    first commit variables

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,17 @@
+'use strict'; 
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+ 
+  if (d === 0) {
+    arr[0] = -b / (2 * a);
+  } else if (d > 0) {
+    arr[0] = (-b + Math.sqrt(d)) / (2 * a);
+    arr[1] = (-b - Math.sqrt(d)) / (2 * a);
+  }
+
+  return arr; 
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
