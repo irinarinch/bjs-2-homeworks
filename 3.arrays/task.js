d1cd9377aefafa00157dd9a28a6db92f92badf19
irinarinch <irinarinch@gmail.com>
2022-08-25 04:43:50 +0800

    third homework to check

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,18 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
-
-  return result; // boolean
+  if (arr1.length === arr2.length) {
+    result = arr1.every((element, index) => index === arr2.indexOf(element));    
+  } else {
+    result = false;
+  }
+ 
+  return result;
 }
 
+
 function advancedFilter(arr) {
-  let resultArr;
+  let resultArr = arr.filter(number => number > 0).filter(number => number % 3 === 0).map(number => number * 10);
 
-  // Ваш код
-
-  return resultArr; // array
+  return resultArr; 
 }
