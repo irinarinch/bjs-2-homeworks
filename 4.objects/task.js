7790b2c53782183ca2cd3b2e96805ec37329e122
irinarinch <irinarinch@gmail.com>
2022-08-29 02:38:47 +0800

    4 homework to check

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,43 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+  if (this.marks === undefined) { 
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+}
+
+Student.prototype.addMarks = function(...args) {
+  if (this.marks === undefined) { 
+    this.marks = [...args];
+  } else {
+    this.marks.push(...args); 
+  }     
+}
+
+Student.prototype.getAverage = function() {
+  let sum = 0;
+  let avg;
+
+  for (let i = 0; i < this.marks.length; i += 1) {
+    sum += this.marks[i];
+    avg = sum / this.marks.length;
+  }
+
+  return avg;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
